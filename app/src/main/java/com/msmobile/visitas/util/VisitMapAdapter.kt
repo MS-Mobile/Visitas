892b10b15e985c58c8cb983c8e7c2a60779fea6f
Fabricio Somini <fabricio.somini@gmail.com>
2026-03-08 23:59:08 -0300

    Refactor VisitMapAdapter to simplify JSON serialization methods

--- app/src/main/java/com/msmobile/visitas/util/VisitMapAdapter.kt
+++ app/src/main/java/com/msmobile/visitas/util/VisitMapAdapter.kt
@@ -2,22 +2,13 @@ package com.msmobile.visitas.util
 
 import com.msmobile.visitas.visit.VisitMapData
 import com.squareup.moshi.JsonAdapter
-import com.squareup.moshi.JsonReader
-import com.squareup.moshi.JsonWriter
 import com.squareup.moshi.Moshi
 
-class VisitMapAdapter(private val moshi: Moshi) : JsonAdapter<List<VisitMapData>?>() {
+class VisitMapAdapter(private val moshi: Moshi) {
     private val adapter: JsonAdapter<List<VisitMapData>?> by lazy(::createAdapter)
 
-    override fun fromJson(p0: JsonReader?): List<VisitMapData>? {
-        return adapter.fromJson(p0)
-    }
-
-    override fun toJson(
-        p0: JsonWriter?,
-        p1: List<VisitMapData>?
-    ) {
-        return adapter.toJson(p0, p1)
+    fun toJson(data: List<VisitMapData>): String? {
+        return adapter.toJson(data)
     }
 
     private fun createAdapter(): JsonAdapter<List<VisitMapData>?> {
