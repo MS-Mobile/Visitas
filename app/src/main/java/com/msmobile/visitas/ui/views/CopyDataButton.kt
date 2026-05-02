3108d35d254ac72d2a72c44a127feed92a7f2019
Fabricio Somini <fabricio.somini@gmail.com>
2026-05-01 22:53:24 -0300

    Add CopyDataButton composable

---
+++ app/src/main/java/com/msmobile/visitas/ui/views/CopyDataButton.kt
@@ -0,0 +1,24 @@
+package com.msmobile.visitas.ui.views
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.res.stringResource
+import com.msmobile.visitas.R
+import com.msmobile.visitas.ui.icons.CopyDataIcon
+
+@Composable
+fun CopyDataButton(show: Boolean, onClick: () -> Unit) {
+    AnimatedVisibility(visible = show) {
+        val label = stringResource(id = R.string.copy_data_content_description)
+        IconButton(
+            modifier = Modifier.semantics { contentDescription = label },
+            onClick = onClick,
+        ) {
+            CopyDataIcon()
+        }
+    }
+}
