dcbf91c4227ad71dd885d087c1decf15487aa4e2
Fabricio Somini <fabricio.somini@gmail.com>
2026-05-07 22:11:40 -0300

    test: update VisitDetailScreenshotTest

--- app/src/screenshotTest/kotlin/com/msmobile/visitas/visit/VisitDetailScreenshotTest.kt
+++ app/src/screenshotTest/kotlin/com/msmobile/visitas/visit/VisitDetailScreenshotTest.kt
@@ -1,7 +1,6 @@
 package com.msmobile.visitas.visit
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import com.android.tools.screenshot.PreviewTest
 import com.msmobile.visitas.ui.theme.PreviewPhone
