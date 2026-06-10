3f970cbf7d57664fe7ce1088bcf1ae0ab0952dc9
fabriciosomini <fabricio.somini@gmail.com>
2026-06-09 23:06:47 -0300

    Add navigationBarsPadding to snackbarPadding extension

--- app/src/main/java/com/msmobile/visitas/util/ModifierExtension.kt
+++ app/src/main/java/com/msmobile/visitas/util/ModifierExtension.kt
@@ -1,12 +1,14 @@
 package com.msmobile.visitas.util
 
 import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.Modifier
 
 fun Modifier.snackbarPadding(): Modifier {
-    return padding(borderPadding)
+    return navigationBarsPadding()
         .imePadding()
+        .padding(borderPadding)
         .padding(bottom = verticalFieldPadding)
         .padding(bottom = verticalFieldPadding + floatingBarBottomPadding)
 }
